6e64d9aafe167c18dd541afbc043730efdd63f3a
Henry <41442529+HenryNorth@users.noreply.github.com>
2022-09-15 13:26:38 +0100

    Create santis-calculator.js

---
+++ projects/santis-calculator/santis-calculator.js
@@ -0,0 +1,19 @@
+const Ingredients = Object.freeze({
+  TacoShell: 1,
+  ShreddedChicken: 2,
+  Toppings: 3,
+  TacoMeat: 4,
+  Tortillas: 5,
+  ShreddedSteak: 6,
+  HotSauce: 7,
+  RefriedBeans: 8,
+  ShreddedCheese: 9,
+  MexicanRice: 10,
+  TortillaChips: 11,
+  Salsa: 12,
+  Queso: 13
+});
+
+const menuItems = [
+  {}
+]
